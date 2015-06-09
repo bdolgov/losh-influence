b08aac1a4d4a3ebc8523c507b74d649d7aca4087
Boris Dolgov <boris@dolgov.name>
2015-06-09 18:30:19 +0300

    Add first version on manager

---
+++ manager/manager.cpp
@@ -0,0 +1,528 @@
+//#include <QtCore>
+#include "manager.hpp"
+
+#include <random>
+#include <iostream>
+#include <fstream>
+
+
+#define FAILS 100
+
+Log logger;
+LogWriter* Log::logWriter = nullptr;
+
+Cell::Cell(Coord _coord, int _size):
+	coord(_coord), size(_size)
+{
+}
+
+Player::Status Player::status() const
+{
+	return m_status;
+}
+
+void Player::setStatus(Player::Status _status)
+{
+	if ((m_status == Waiting && _status != Conquest)
+		|| (m_status == Conquest && _status != Fortify)
+		|| (m_status == Fortify && _status != Waiting))
+	{
+		logger() << this << " пытается сменить состояние на неразрешенное";
+		throw LogicError();
+	}
+	m_status = _status;
+}
+
+void Player::failed()
+{
+	++n_failed;
+	if (n_failed == FAILS)
+	{
+		logger() << this << " отправил слишком много ошибочных команд и дисквалифицирован.";
+		m_status = Broken;
+	}
+}
+
+void Game::loadMap(const std::string& path)
+{
+	if(players.empty())
+	{
+		throw std::logic_error("You must add players before loading the map!");
+	}
+	std::ifstream f(path);
+	int n; f >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		int x, y, s, m;
+		f >> x >> y >> s >> m;
+		cells.insert(std::make_pair(Coord(x, y), std::unique_ptr<Cell>(new Cell({x, y}, s))));
+		for (int j = 0; j < m; ++j)
+			f >> x >> y;
+	}
+	f.seekg(0);
+	f >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		int x, y, s, m;
+		f >> x >> y >> s >> m;
+		Cell *c = findCell(x, y);
+		for (int j = 0; j < m; ++j)
+		{
+			f >> x >> y;
+			c->neigh.push_back(findCell(x, y));
+		}
+	}
+	for (auto &i : players)
+	{
+		int x, y;
+		f >> x >> y;
+		findCell(x, y)->owner = &*i;
+		findCell(x, y)->power = 2;
+	}
+}
+
+void Game::addPlayer(PlayerEngine *player)
+{
+	if (!cells.empty())
+	{
+		throw std::logic_error("You must not add players after loading the map!");
+	}
+	players.emplace_back(new Player(players.size() + 1, player));
+}
+
+bool Cell::visibleTo(Player* player)
+{
+	if (owner == player) return true;
+	for (auto i : neigh)
+	{
+		if (i->owner == player) return true;
+	}
+	return false;
+}
+
+PlayerEngine *Player::engine() const
+{
+	return m_engine;
+}
+
+std::ostream& operator<<(std::ostream& o, const Coord &c)
+{
+	o << c.x << " " << c.y;
+	return o;
+}
+
+bool Player::needHello()
+{
+	if (sentHello)
+		return false;
+	sentHello = true;
+	return true;
+}
+
+std::ostream& operator<<(std::ostream& o, Player *p)
+{
+	if (!p)
+	{
+		o << "Player (null)";
+	}
+	else
+	{
+		o << "Player " << p->id();
+	}
+	return o;
+}
+
+void TextPlayer::sendHello(int id)
+{
+	write("HELLO " + std::to_string(id));
+}
+
+void TextPlayer::sendError()
+{
+	write("ERROR");
+}
+
+void TextPlayer::sendOk()
+{
+	write("OK");
+}
+
+void TextPlayer::sendConquestResult(Game::AttackResult result, int pa, int pb)
+{
+	write((result == Game::Won ? "SUCCESS " : "FAIL ") + std::to_string(pa) + " " + std::to_string(pb));
+}
+
+void TextPlayer::sendGameResult(int result)
+{
+	if (result < 0)
+		write("LOST");
+	else if (result == 0)
+		write("CONTINUE");
+	else 
+		write("WON");
+}
+
+void TextPlayer::sendMap(const std::vector<Cell*>& cells, const std::vector<int>& players)
+{
+	std::stringstream text;
+	text << cells.size() << "\n";
+	for (auto i : cells)
+	{
+		text << i->coord << " " << i->size << " " << (i->owner ? i->owner->id() : 0) << " " << i->power << " " << i->neigh.size() << "\n";
+		for (auto j : i->neigh)
+		{
+			text << j->coord << "\n";
+		}
+	}
+	text << players[1] << " " << players[2] << " " << players[3] << " " << players[4];
+	write(text.str());
+}
+
+void StdioPlayer::write(const std::string& s)
+{
+	std::cout << s + "\n";
+}
+
+std::string StdioPlayer::read()
+{
+	std::string incoming;
+	std::getline(std::cin, incoming);
+	return incoming;
+}
+
+PlayerEngine::Command TextPlayer::getCommand()
+{
+	std::string incoming = read();
+	if (incoming.empty())
+		return Command();
+	logger() << "Принята команда " << incoming; 
+	std::stringstream ss(incoming);
+	Command ret;
+	std::string cmd;
+	ss >> cmd;
+	if (cmd == "BEGIN")
+	{
+		ss >> cmd;
+		if (cmd == "CONQUEST")
+		{
+			ret.type = ret.BeginConquest;
+		}
+		else if (cmd == "FORTIFICATION")
+		{
+			ret.type = ret.BeginFortification;
+		}
+		else
+		{
+			logger() << "Неизвестная команда";
+			throw LogicError();
+		}
+	}
+	else if (cmd == "MAP")
+	{
+		ret.type = ret.Map;
+	}
+	else if (cmd == "CONQUEST")
+	{
+		ret.type = ret.Conquest;
+		ret.args.resize(4);
+		ss >> ret.args[0] >> ret.args[1] >> ret.args[2] >> ret.args[3];
+	}
+	else if (cmd == "FORTIFY")
+	{
+		ret.type = ret.Fortify;
+		ret.args.resize(2);
+		ss >> ret.args[0] >> ret.args[1];
+	}
+	else if (cmd == "PASS")
+	{
+		ret.type = ret.Pass;
+	}
+	else
+	{
+		logger() << "Неизвестная команда";
+		throw LogicError();
+	}
+	return ret;
+}
+
+
+int getAttack(int power)
+{
+	static std::mt19937 generator;
+	static std::uniform_int_distribution<int> distribution(1,6);
+	int ret = 0;
+	for (int i = 0; i < power; ++i)
+		ret += distribution(generator);
+	return ret;
+}
+
+Log::LogStream::LogStream(Log& _logger):
+	logger(_logger),
+	record(new std::stringstream)
+{
+}
+
+Log::LogStream::~LogStream()
+{
+	logger.addComment(record->str());
+}
+
+Log::Log()
+{
+	if (!logWriter)
+	{
+		logWriter = new StderrLogWriter();
+	}
+}
+
+Log::LogStream Log::operator()()
+{
+	return LogStream(*this);
+}
+
+void Log::addCell(Cell* cell)
+{
+	logWriter->addCell(cell);
+}
+
+void Log::addComment(const std::string& s)
+{
+	logWriter->addComment(s);
+}
+
+void Game::addPower(Player* player, Cell* cell)
+{
+	logger() << player << " пытается добавить силу в клетку " << cell->coord;
+	if (player->status() != player->Fortify)
+	{
+		logger() << "Ошибка: игрок не находится в стадии укрепления";
+		throw LogicError();
+	}
+	if (cell->owner != player)
+	{
+		logger() << "Ошибка: клеткой владеет " << cell->owner;
+		throw LogicError();
+	}
+	if (cell->power == cell->size)
+	{
+		logger() << "Ошибка: клетка превысит свой размер " << cell->size;
+		throw LogicError();
+	}
+	++cell->power;
+	logger.addCell(cell);
+	logger() << "Успешно: теперь сила клетки равна " << cell->power << " / " << cell->size;
+}
+
+Game::AttackResult Game::attack(Player* player, Cell* cell, Cell* other)
+{
+	logger() << player << " пытается атаковать клеткой " << cell->coord << " клетку " << other->coord;
+	if (player->status() != player->Conquest)
+	{
+		logger() << "Ошибка: игрок не находится в стадии атаки";
+		throw LogicError();
+	}
+	if (player != cell->owner)
+	{
+		logger() << "Ошибка: атакующей клеткой владеет другой игрок " << cell->owner;
+		throw LogicError();
+	}
+	if (cell->power < 2)
+	{
+		logger() << "Ошибка: сила клетки меньше 2";
+		throw LogicError();
+	}
+	if (std::find(cell->neigh.begin(), cell->neigh.end(), other) == cell->neigh.end())
+	{
+		logger() << "Ошибка: атакующая и атакуемая клетка не являются соседями";
+		throw LogicError();
+	}
+	if (other->owner == player)
+	{
+		logger() << "Ошибка: атакующий игрок владеет атакуемой клеткой";
+		throw LogicError();
+	}
+	AttackResult result;
+	if (!other->owner)
+	{
+		logger() << "Успешно: захвачена ничья клетка";
+		other->owner = cell->owner;
+		other->power = std::min(cell->power - 1, other->size);
+		cell->power = 1;
+		result = Won;
+	}
+	else
+	{
+		logger() << "Атакумая клетка принадлежит игроку " << other->owner;
+		int attackA = getAttack(cell->power), attackB = getAttack(other->power);
+		logger() << "Атакующая клетка: " << attackA << ", атакуемая клетка: " << attackB;
+		if (attackA > attackB)
+		{
+			logger() << "Атакующий побеждает";
+			other->owner = cell->owner;
+			other->power = std::min(std::max(1, cell->power - (attackB + 5) / 6 - 1), other->size);
+			cell->power = 1;
+			result = Won;
+		}
+		else if (attackA == attackB)
+		{
+			logger() << "Ничья!";
+			cell->power = 1;
+			other->power = 1;
+			result = Draw;
+		}
+		else
+		{
+			logger() << "Атакуемый побеждает";
+			cell->power = 1;
+			other->power = std::max(1, other->power - (attackA + 5) / 6);
+			result = Lost;
+		}
+	}
+	logger.addCell(cell);
+	logger.addCell(other);
+	return result;
+}
+
+void Game::processPlayer(Player* player)
+{
+	if (player->status() == player->Broken)
+	{
+		logger() << "Ход " << player << " пропускается, так как он дисквалифицирован";
+		return;
+	}
+	if (player->status() != player->Waiting)
+	{
+		logger() << "Внутреннаяя ошибка: processPlayer при статусе " << static_cast<int>(player->status());
+		throw std::runtime_error("RUNTIME ERROR");
+	}
+	logger() << "Начинается ход " << player;
+	PlayerEngine *pe = player->engine();
+	if (player->needHello())
+	{
+		pe->sendHello(player->id());
+	}
+	else
+	{
+		pe->sendGameResult(0);
+	}
+	while (player->status() != player->Broken)
+	{
+		try
+		{
+			auto cmd = pe->getCommand();
+			if (cmd.type == cmd.Null)
+			{
+				logger() << player << " дисквалифицирован в связи с остановкой программы";
+				player->m_status = player->Broken;
+				break;
+			}
+			else if (cmd.type == cmd.Map)
+			{
+				logger() << player << " запрашивает карту";
+				std::vector<int> pplayers(5);
+				std::vector<Cell*> pcells;
+				for (auto &i : cells)
+				{
+					if (i.second->visibleTo(player))
+					{
+						pcells.push_back(&*(i.second));
+					}
+					if (i.second->owner)
+					{
+						++pplayers[i.second->owner->id()];
+					}
+				}
+				pe->sendMap(pcells, pplayers);
+			}
+			else if (cmd.type == cmd.BeginConquest)
+			{
+				player->setStatus(player->Conquest);
+				pe->sendOk();
+				logger() << player << " перешел в режим завоевания";
+
+			}
+			else if (cmd.type == cmd.BeginFortification)
+			{
+				player->setStatus(player->Fortify);
+				player->fortificationLimit = 0;
+				for (auto &i : cells)
+				{
+					if (i.second->owner == player)
+						++player->fortificationLimit;
+				}
+				pe->sendOk();
+				logger() << player << " перешел в режим укрепления. Лимит: " << player->fortificationLimit;
+			}
+			else if (cmd.type == cmd.Pass)
+			{
+				player->setStatus(player->Waiting);
+				break;
+			}
+			else if (cmd.type == cmd.Fortify)
+			{
+				if (!player->fortificationLimit)
+				{
+					logger() << player << " больше не может укреплять свои клетки";
+					throw LogicError();
+				}
+				addPower(player, findCell(cmd.args[0], cmd.args[1]));
+				pe->sendOk();
+				--player->fortificationLimit;
+				logger() << player << " может выполнить еще " << player->fortificationLimit << " улучшений";
+			}
+			else if (cmd.type == cmd.Conquest)
+			{
+				Cell *a = findCell(cmd.args[0], cmd.args[1]), *b = findCell(cmd.args[2], cmd.args[3]);
+				auto result = attack(player, a, b);
+				pe->sendConquestResult(result, a->power, b->power);
+			}
+			else if (cmd.type == cmd.Pass)
+			{
+				player->setStatus(player->Waiting);
+			}
+		}
+		catch (LogicError&)
+		{
+			logger() << player << ": команда завершилась ошибкой";
+			player->failed();
+			pe->sendError();
+		}
+	}
+}
+
+Cell* Game::findCell(int x, int y)
+{
+	auto it = cells.find({x, y});
+	if (it != cells.end()) return &*(it->second);
+	logger() << "Клетка с координатами " << x << " " << y << " не существует!";
+	throw LogicError();
+}
+
+void Game::round()
+{
+	for (auto &i : players)
+	{
+		processPlayer(&*i);
+	}
+}
+
+void StderrLogWriter::addCell(Cell*)
+{
+}
+
+void StderrLogWriter::addComment(const std::string& s)
+{
+	std::cerr << s << std::endl;
+}
+
+#if 0
+int main(int ac, char** av)
+{
+	Game game;
+	game.addPlayer(new StdioPlayer);
+	game.addPlayer(new StdioPlayer);
+	game.loadMap("../maps/map01.txt");
+	for (int i = 0; i < 10; ++i)
+		game.round();
+}
+#endif
---
+++ manager/manager.hpp
@@ -0,0 +1,224 @@
+#ifndef MANAGER_HPP
+#define MANAGER_HPP
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <memory>
+
+#define TIMEOUT 1000
+
+struct Coord
+{
+	int x, y;
+	Coord(int _x, int _y): x(_x), y(_y) {}
+	bool operator<(const Coord& other) const
+	{
+		return x != other.x ? x < other.x : y < other.y;
+	}
+	friend std::ostream& operator<<(std::ostream& o, const Coord &c);
+};
+
+class Player;
+class Cell;
+class PlayerEngine;
+
+struct LogicError : std::logic_error
+{
+	LogicError(): std::logic_error("Game logic error") {}
+};
+
+class Player
+{
+	public:
+		enum Status { Waiting, Fortify, Conquest, Broken };
+		Status status() const;
+		void setStatus(Status status);
+
+		int id() const { return m_id; }
+
+		bool needHello();
+		void failed();
+
+		PlayerEngine *engine() const;
+
+		friend class Game;
+		friend std::ostream& operator<<(std::ostream& o, Player *p);
+		
+	private:
+		int m_id;
+		PlayerEngine *m_engine;
+		Status m_status = Waiting;
+		bool sentHello = false;
+		Player(int _id, PlayerEngine* _engine): m_id(_id), m_engine(_engine) {}
+		int n_failed = 0;
+		int fortificationLimit = 0;
+};
+
+class Cell
+{
+	public:
+		Coord coord;
+		std::vector<Cell*> neigh;
+		Player *owner = nullptr;
+		int size = 0;
+		int power = 0;
+
+		bool visibleTo(Player *player);
+
+		Cell(Coord _coord, int _size);
+};
+
+class Game
+{
+	private:
+		std::map<Coord, std::unique_ptr<Cell>> cells;
+		std::vector<std::unique_ptr<Player>> players;
+
+		Cell* findCell(int x, int y);
+
+	public:
+		enum AttackResult { Null, Won, Draw, Lost };
+		void addPlayer(PlayerEngine* player);
+
+		void addPower(Player* player, Cell* cell);
+		Game::AttackResult attack(Player* player, Cell* cell, Cell* other);
+
+		void processPlayer(Player* player);
+		bool gameFinished();
+
+		void loadMap(const std::string& path);
+		void round();
+};
+
+class PlayerEngine
+{
+	public:
+		struct Command
+		{
+			enum Type { Null, BeginConquest, Conquest, BeginFortification, Fortify, Map, Pass } type = Null;
+			std::vector<int> args;
+		};
+		
+		virtual ~PlayerEngine() {}
+
+		virtual void sendHello(int id) = 0;
+		virtual void sendError() = 0;
+		virtual void sendOk() = 0;
+		virtual void sendConquestResult(Game::AttackResult result, int pa, int pb) = 0;
+		virtual void sendGameResult(int result) = 0;
+		virtual void sendMap(const std::vector<Cell*>& cells, const std::vector<int>& players) = 0;
+		virtual Command getCommand() = 0;
+};
+
+class LogWriter
+{
+	public:
+		virtual void addCell(Cell* cell) = 0;
+		virtual void addComment(const std::string& s) = 0;
+		virtual ~LogWriter() {}
+};
+
+class Log
+{
+	private:
+		class LogStream
+		{
+			private:
+				Log& logger;
+				std::shared_ptr<std::stringstream> record;
+
+			public:
+				LogStream(Log& _logger);
+				~LogStream();
+
+				template<class T>
+				LogStream& operator<<(const T& value)
+				{
+					(*record) << value;
+					return *this;
+				}
+		};
+		static LogWriter* logWriter;
+
+	public:
+		Log();
+		LogStream operator()();
+		void addCell(Cell* cell);
+		void addComment(const std::string& s);
+		static void setLogWriter(LogWriter* lw) { logWriter = lw; }
+};
+
+class TextPlayer : public PlayerEngine
+{
+	public:
+		virtual void sendHello(int id);
+		virtual void sendError();
+		virtual void sendOk();
+		virtual void sendConquestResult(Game::AttackResult result, int pa, int pb);
+		virtual void sendGameResult(int result);
+		virtual void sendMap(const std::vector<Cell*>& cells, const std::vector<int>& players);
+		virtual Command getCommand();
+	protected:
+		virtual void write(const std::string& s) = 0;
+		virtual std::string read() = 0;
+};
+
+class StdioPlayer : public TextPlayer
+{
+	protected:
+		void write(const std::string& s);
+		std::string read();
+};
+
+#ifdef QT_CORE_LIB
+class QIODevicePlayer : public TextPlayer
+{
+	public:
+		QIODevicePlayer(QIODevice *_io): io(_io) {}
+
+	protected:
+		void write(const std::string& s)
+		{
+			io->write(s.c_str());
+		}
+
+		std::string read()
+		{
+			if (!io->canReadLine())
+			{
+				QEventLoop evloop;
+				QTimer timer;
+				timer.start(TIMEOUT);
+				QObject::connect(&timer, &QTimer::timeout, &evloop, &QEventLoop::quit);
+				QObject::connect(io, &QProcess::readyRead, &evloop, &QEventLoop::quit);
+				evloop.exec();
+			}
+			if (!io->canReadLine())
+			{
+				return "";
+			}
+			else
+			{
+				return io->readLine().data();
+			}
+		}
+
+	private:
+		QIODevice *io;
+};
+#endif
+
+class StderrLogWriter : public LogWriter
+{
+	public:
+		void addCell(Cell*);
+		void addComment(const std::string& s);
+};
+
+LogWriter *logWriter = nullptr;
+
+extern Log logger;
+
+#endif
